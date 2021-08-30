7b68f1459cd99c1ec36c8719077871947df084f0
nakamozu <mozu.mo16zu@gmail.com>
2021-08-30 20:07:05 +0900

    CSS変更（ハンバーガー、ビジュアル）

--- js/main.js
+++ js/main.js
@@ -4,14 +4,33 @@ $(function(){
         section : ".box",
         scrollbars: false,
         scrollSpeed: 800,
-    
 
-        //以下、ページネーション設定
-        before:function(i,panels) {
-        var ref = panels[i].attr("data-section-name");
-        $(".pagination-item .active").removeClass("active");
-        $(".pagination-item").find("a[href=\"#" + ref + "\"]").addClass("active");
+    //以下、ページネーション設定
+      before:function(i,panels) {  // [i]は表示されている画面のIndex番号（idx）
+        var ref = panels[i].attr("data-section-name"); // attrは要素の属性を処理する際に使用
+        $(".pagination .active").removeClass("active");
+        $(".pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
       },
+      afterRender:function() {
+        var pagination = "<ul class=\"pagination\">";
+        var activeClass = "";
+        $(".box").each(function(i) {//1ページスクロールさせたいエリアクラス名を指定
+          activeClass = "";
+          if(i===$.scrollify.currentIndex()) {
+            activeClass = "active";
+          }
+          pagination += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
+        });
+        pagination += "</ul>";
+
+        $("#home").append(pagination);//headerにページネーションを表示
+        $(".pagination a").on("click",$.scrollify.move);
+        $(".btn-scroll a").on("click",$.scrollify.move);
+      }
     });
 
+    // ハンバーガーメニュー
+    $(".openbtn-area").click(function(){
+      $(".openbtn-circle").toggleClass('active');
+    });
 });
